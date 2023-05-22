7c9b99a13509227697f66cbd84da57081b23c4f1
DDang <k3h1i013@gmail.com>
2023-05-22 20:17:14 +0900

    feat: Added Font File

--- Package.swift
+++ Package.swift
@@ -16,8 +16,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "iOS-TripBook-Component-Package",
-            targets: ["iOS-TripBook-Component-Package"]),
+            name: "Util",
+            targets: ["Util"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -27,10 +28,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "iOS-TripBook-Component-Package",
-            dependencies: []),
-        .testTarget(
-            name: "iOS-TripBook-Component-PackageTests",
-            dependencies: ["iOS-TripBook-Component-Package"]),
+            name: "Util",
+            resources: [
+                .process("Resource/Font")
+            ]
+        ),
     ]
 )
