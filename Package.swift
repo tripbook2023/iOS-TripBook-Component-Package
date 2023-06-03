8f1cd058c0b0f2c46b4487cd58285aa49cea9636
DDang <k3h1i013@gmail.com>
2023-06-03 13:44:26 +0900

    Updated Package
    Util package to TBUtil package

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TBUtil",
-            targets: ["Util"]
+            targets: ["TBUtil"]
         ),
     ],
     dependencies: [
@@ -29,7 +29,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Util",
+            name: "TBUtil",
             resources: [
                 /// Font 리소스 폴더 지정
                 .process("Resource/Font")
